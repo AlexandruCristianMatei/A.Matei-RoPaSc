db9ea42edd84b2c09121126662ade0840d2ac674
SurgicalGoblin92 <alexandru.matei0210@gmail.com>
2020-04-21 16:47:57 +0300

    RoPaSc_smallChanges

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function play() {
     let arr = ['rock', 'paper', 'scissors'];
-    //            0       1         2
+    //           0        1          2
     let AIchoice = Math.floor(Math.random() * arr.length);
     let MYchoice = Math.floor(Math.random() * arr.length);
 
-    if (AIchoice == 0 && MYchoice == 1 || AIchoice == 1 && MYchoice == 2 || AIchoice == 2 && MYchoice == 0) {
+    if (AIchoice === 0 && MYchoice === 1 || AIchoice === 1 && MYchoice === 2 || AIchoice === 2 && MYchoice === 0) {
         console.log('Computer has chosen ' + arr[AIchoice]);
         console.log('User has chosen ' + arr[MYchoice]);
         console.log('User first, computer second!');
     } else
-    if (AIchoice == 1 && MYchoice == 0 || AIchoice == 2 && MYchoice == 1 || AIchoice == 0 && MYchoice == 2) {
+    if (AIchoice === 1 && MYchoice === 0 || AIchoice === 2 && MYchoice === 1 || AIchoice === 0 && MYchoice === 2) {
         console.log('Computer has chosen ' + arr[AIchoice]);
         console.log('User has chosen ' + arr[MYchoice]);
         console.log('Computer is just lucky today!');
